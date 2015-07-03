e7378b1cf0425098521c43d3a0aadaa164f3a2c0
gustavolivas <gusoliv3ira@gmail.com>
2015-07-03 00:54:42 -0300

    Game with a car image

--- pygame/game.py
+++ pygame/game.py
@@ -1,12 +1,30 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 #Resolução de tela
 pygame.display.set_caption('A bit Racey')
 #titulo da tela
 clock = pygame.time.Clock()
 #clock do jogo
+
+carImg = pygame.image.load('img/racecar.png')
+
+def car(x,y):
+	gameDisplay.blit(carImg,(x,y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+x_change = 0
 
 crashed = False
 
@@ -17,7 +35,21 @@ while not crashed:
 		if event.type == pygame.QUIT:
 			crashed = True
 
-		print(event)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				x_change = -5
+			elif event.key == pygame.K_RIGHT:
+				x_change = 5
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				x_change = 0
+
+	x += x_change
+
+	gameDisplay.fill(white)
+	car(x, y)
+		#print(event)
 
 	pygame.display.update()
 	clock.tick(60)
